3a6041c465053d5588efcb7d1719b6fc8486a825
HarunRasheed7 <harunz96@gmail.com>
2020-08-11 12:49:16 +0530

    08/11/2020

--- src/test/java/com/telecom/stepdefinition/AddTariffplanSteps.java
+++ src/test/java/com/telecom/stepdefinition/AddTariffplanSteps.java
@@ -1,5 +1,7 @@
 package com.telecom.stepdefinition;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
 
 public class AddTariffplanSteps {
 	static WebDriver driver;
@@ -39,6 +42,32 @@ public class AddTariffplanSteps {
 	    driver.findElement(By.id("inter_charges")).sendKeys("3");
 	    driver.findElement(By.id("sms_charges")).sendKeys("2");
 	}
+	@When("User filling all the details in tariff plans using one dimensional list")
+	public void user_filling_all_the_details_in_tariff_plans_using_one_dimensional_list(DataTable dT) throws InterruptedException {
+		handleFrame();
+		List<String> li = dT.asList();
+	    driver.findElement(By.id("rental1")).sendKeys(li.get(0));
+	    driver.findElement(By.id("local_minutes")).sendKeys(li.get(1));
+	    driver.findElement(By.id("inter_minutes")).sendKeys(li.get(2));
+	    driver.findElement(By.id("sms_pack")).sendKeys(li.get(3));
+	    driver.findElement(By.id("minutes_charges")).sendKeys(li.get(4));
+	    driver.findElement(By.id("inter_charges")).sendKeys(li.get(5));
+	    driver.findElement(By.id("sms_charges")).sendKeys(li.get(6));
+	}
+	@When("User filling all the details in tariff plans using two dimensional list")
+	public void user_filling_all_the_details_in_tariff_plans_using_two_dimensional_list(DataTable dT) throws InterruptedException {
+		handleFrame();
+		List<List<String>> li = dT.asLists();
+		driver.findElement(By.id("rental1")).sendKeys(li.get(0).get(6));
+	    driver.findElement(By.id("local_minutes")).sendKeys(li.get(1).get(0));
+	    driver.findElement(By.id("inter_minutes")).sendKeys(li.get(3).get(1));
+	    driver.findElement(By.id("sms_pack")).sendKeys(li.get(3).get(2));
+	    driver.findElement(By.id("minutes_charges")).sendKeys(li.get(0).get(3));
+	    driver.findElement(By.id("inter_charges")).sendKeys(li.get(2).get(4));
+	    driver.findElement(By.id("sms_charges")).sendKeys(li.get(3).get(5));
+	}
+
+
 
 	@When("User clicks on add tariff submit button")
 	public void user_clicks_on_add_tariff_submit_button() {
@@ -53,6 +82,7 @@ public class AddTariffplanSteps {
 	   String text = driver.findElement(By.xpath("//h2[@style='text-align: center;font-weight: 700;font-size:28px;']")).getText();
 	   System.out.println(text);
 	}
+	
 
  public void handleFrame() throws InterruptedException {
 	 Thread.sleep(5000);
--- src/test/resources/Feature File/AddTariff.feature
+++ src/test/resources/Feature File/AddTariff.feature
@@ -7,3 +7,22 @@ Feature: Adding tariff
     When User filling all the details in tariff plans
     And User clicks on add tariff submit button
     Then User will be displayed with congratulation msg
+
+ Scenario: Adding Tariff plan using one dimensional list
+    Given User launch the telecom website for adding tariff
+    And User clicks on Add Tariff
+    When User filling all the details in tariff plans using one dimensional list
+      | 2000 | 100 | 500 | 10 | 5 | 3 | 1 |
+    And User clicks on add tariff submit button
+    Then User will be displayed with congratulation msg
+
+  Scenario: Adding Tariff plan using two dimensional list
+    Given User launch the telecom website for adding tariff
+    And User clicks on Add Tariff
+    When User filling all the details in tariff plans using two dimensional list
+      | 2000 | 100 | 500 |  10 |  5 | 3 | 1 |
+      | 3000 | 200 |  70 | 300 | 20 | 9 | 9 |
+      | 2000 | 100 | 500 |   7 |  5 | 8 | 8 |
+      | 3000 | 200 |  70 | 300 | 20 | 5 | 7 |
+    And User clicks on add tariff submit button
+    Then User will be displayed with congratulation msg
